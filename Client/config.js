e3af70fdc605f0fe1103d5f0aa07c9517cc06c05
Zeno <zeno.user@y3t2.fr>
2025-06-20 01:30:36 +0200

    Update config.js

--- Client/config.js
+++ Client/config.js
@@ -3,7 +3,7 @@ module.exports = {
     prefix: '+',              
     color: '#422244',        
     footer: '© Powered by d_1114 and y3t2 ', 
-    WEATHER_API_KEY: '787a495512a63a497d8feac224c1d72e', 
+    WEATHER_API_KEY: '', 
     link: 'https://discord.gg/PK6tuf8amW', 
     dev: 'd_1114 and y3t2' ,           
     staff: [
